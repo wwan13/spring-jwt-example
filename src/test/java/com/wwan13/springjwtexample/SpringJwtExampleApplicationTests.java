6dc4d85dbe7e1fd6608bbe289a16ac599b1b8eb3
wwan13 <wwan13@naver.com>
2023-07-02 22:46:34 +0900

    feat : 프로젝트 생성

---
+++ src/test/java/com/wwan13/springjwtexample/SpringJwtExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wwan13.springjwtexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
