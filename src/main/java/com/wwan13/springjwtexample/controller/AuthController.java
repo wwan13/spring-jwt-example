d0c4a8c1249fd2b5bc1edcd6d5992cfae9f45bb9
wwan13 <wwan13@naver.com>
2023-07-03 17:39:32 +0900

    feat : POST "/auth/signup" 구현, login 컨트롤러에서 로그인 로직 분리

--- src/main/java/com/wwan13/springjwtexample/controller/AuthController.java
+++ src/main/java/com/wwan13/springjwtexample/controller/AuthController.java
@@ -1,20 +1,20 @@
 package com.wwan13.springjwtexample.controller;
 
 import com.wwan13.springjwtexample.dto.LoginDto;
+import com.wwan13.springjwtexample.dto.SignupDto;
 import com.wwan13.springjwtexample.dto.TokenDto;
 import com.wwan13.springjwtexample.jwt.JwtFilter;
 import com.wwan13.springjwtexample.jwt.TokenProvider;
+import com.wwan13.springjwtexample.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -23,19 +23,12 @@ import javax.validation.Valid;
 @RequestMapping(value = "/auth")
 public class AuthController {
 
-    private final TokenProvider tokenProvider;
-    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final MemberService memberService;
 
     @PostMapping(value = "/login")
     public ResponseEntity login(@Valid @RequestBody LoginDto loginDto) {
 
-        UsernamePasswordAuthenticationToken authenticationToken =
-                new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword());
-
-        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-
-        String jwt = tokenProvider.createToken(authentication);
+        String jwt = memberService.login(loginDto);
 
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
@@ -43,4 +36,20 @@ public class AuthController {
         return ResponseEntity.ok().headers(httpHeaders).body(new TokenDto(jwt));
     }
 
+    @PostMapping(value = "/signup")
+    public ResponseEntity signup(@Valid @RequestBody SignupDto signupDto) {
+        return ResponseEntity.ok().body(memberService.signup(signupDto));
+    }
+
+    @GetMapping(value = "/user")
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')") // user, admin 권한을 가진 토큰만 호츨 가능
+    public ResponseEntity getMyUserInfo() {
+        return ResponseEntity.ok().body(memberService.getCurrentUserWithAuthorities());
+    }
+
+    @GetMapping(value = "/user/{username}")
+    @PreAuthorize("hasAnyRole('ADMIN')") // admin 권한을 가진 토큰만 호츨 가능
+    public ResponseEntity getUserInfo(@PathVariable String username) {
+        return ResponseEntity.ok().body(memberService.getUserWithAuthorities(username));
+    }
 }
