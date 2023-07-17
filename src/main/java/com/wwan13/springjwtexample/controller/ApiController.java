effcb5f0ea48899fdb6671300abbe6dba9627fb6
wwan13 <wwan13@naver.com>
2023-07-17 19:35:24 +0900

    feat : controller test

---
+++ src/main/java/com/wwan13/springjwtexample/controller/ApiController.java
@@ -0,0 +1,24 @@
+package com.wwan13.springjwtexample.controller;
+
+import com.wwan13.springjwtexample.entity.Member;
+import com.wwan13.springjwtexample.util.CurrentUser;
+import com.wwan13.springjwtexample.util.SecurityUtil;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+    @GetMapping("/sample")
+    public ResponseEntity<String> sampleApi(@CurrentUser User user) {
+
+        System.out.println(user.getUsername());
+        return ResponseEntity.ok().body("sample");
+    }
+
+}
